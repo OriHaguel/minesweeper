36c6300f368d99323d5bdc5086ed5eb7707a545d
OriHaguel <159137741+OriHaguel@users.noreply.github.com>
2024-02-08 15:50:31 +0200

    gameplay should work

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+'use strict'
 let gLevel = {
     size: 4,
     Mine: 2
@@ -16,13 +16,12 @@ function checkGameOver() {
     if ((gLevel.size ** 2) - gLevel.Mine === gGame.shownCount && gLevel.Mine === gGame.markedCount) {
         console.log('you won!!!')
         gGame.isOn = false
-        
     }
 }
-
 let gBoard = createBoard(gLevel.size)
 
 function begginer() {
+    gGame.isOn = true
     gGame.shownCount = 0
     gGame.markedCount = 0
     gLevel.size = 4
@@ -34,6 +33,7 @@ function begginer() {
 }
 
 function medium() {
+    gGame.isOn = true
     gGame.shownCount = 0
     gGame.markedCount = 0
     gLevel.size = 8
@@ -45,6 +45,7 @@ function medium() {
 }
 
 function expert() {
+    gGame.isOn = true
     gGame.shownCount = 0
     gGame.markedCount = 0
     gLevel.size = 12
@@ -54,11 +55,11 @@ function expert() {
     howManyMines(gLevel.Mine)
     renderCountGamerNegs(gBoard)
     renderBoard(gBoard)
-    // console.log(gLevel.size ** 2 - gLevel.Mine)
 
 }
 
 function onInitGame() {
+    gGame.isOn = true
     gGame.shownCount = 0
     gGame.markedCount = 0
     gLevel.size = 4
@@ -67,9 +68,8 @@ function onInitGame() {
     howManyMines(gLevel.Mine)
     renderCountGamerNegs(gBoard)
     renderBoard(gBoard)
-    
-}
 
+}
 
 function howManyMines(num) {
     for (var i = 0; i < num; i++) {
@@ -91,16 +91,14 @@ function expandShown(board, elCell, row, col) {
                 if (elCell.innerText === MINES || elCell.innerText === FLAG) continue
                 if (countMinesAroundCell(board, row, col) === 0) {
                     if (elCell.classList[1] === `cell-${row}-${col}`) {
-                        // console.log(`cell-${row}-${col}`)
                         downCell.innerText = gBoard[i][j].minesAroundCount
-                        // console.log(downCell.innerText)
                         downCell.style.color = 'rgb(121, 121, 255)'
+                        downCell.style.opacity = '100'
+                        
                         if (gBoard[i][j].isShown === false) {
                             gBoard[i][j].isShown = true
                             gGame.shownCount++
                         }
-
-
                     }
                 }
             }
@@ -110,82 +108,84 @@ function expandShown(board, elCell, row, col) {
 
 function onCellClicked(elCell, i, j) {
 
+    if (!gGame.isOn) return
     if (elCell.innerText === '0') return
 
     if (elCell.innerText === FLAG) {
         elCell.style.color = 'rgb(121, 121, 255)'
+        elCell.style.opacity = '100'
+
         return
     }
-
 
     if (elCell.innerText !== MINES) {
         elCell.innerText = gBoard[i][j].minesAroundCount
         elCell.style.color = 'rgb(121, 121, 255)'
+        elCell.style.opacity = '100'
         if (gBoard[i][j].isShown === false) {
             gBoard[i][j].isShown = true
             gGame.shownCount++
         }
-
-
     }
+
     if (elCell.innerText === MINES) {
         elCell.style.color = 'blue'
+        elCell.style.opacity = '100'
         gBoard[i][j].isShown = true
-
+        gGame.isOn = false
+        isLost()
+        console.log('you lost')
     }
 
-
-
-
-
-
-
-
     expandShown(gBoard, elCell, i, j)
-    // console.log(elCell)
-    // console.log(gBoard)
-    // console.log(gBoard[i][j])
-    // console.log(gGame.shownCount, gLevel.Mine)
     checkGameOver()
 }
 
 function onCellMarked(event, i, j) {
     event.preventDefault()
     const cell = event.target
-
-    if (cell.innerText === FLAG && gBoard[i][j].isMarked === true) {
+    if (!gGame.isOn) return
+    
+    if (cell.innerText === FLAG && gBoard[i][j].isMarked === true && gBoard[i][j].isMine === true) {
         cell.innerText = MINES
         gBoard[i][j].isMarked = false
         cell.style.color = 'transparent'
+        cell.style.opacity = '100'
+        
     } else if (cell.innerText === FLAG) {
         cell.innerText = ''
         gBoard[i][j].isMarked = false
 
+    
     } else if (cell.innerText === '') {
         cell.innerText = FLAG
         gBoard[i][j].isMarked = true
         cell.style.color = 'red'
+        cell.style.opacity = '100'
     } else if (cell.innerText === MINES) {
         cell.innerText = FLAG
         gBoard[i][j].isMarked = true
         cell.style.color = 'red'
+        cell.style.opacity = '100'
     }
 
+    if (cell.innerText === FLAG && gBoard[i][j].isMarked === true && gBoard[i][j].isMine === true) {
+        gGame.markedCount++
+    
+    } else if (gBoard[i][j].isMine === true) {
+        gGame.markedCount--
+    }
 
-  if (cell.innerText === FLAG && gBoard[i][j].isMarked === true && gBoard[i][j].isMine === true ) {
-gGame.markedCount++
-  }  else if(gBoard[i][j].isMine === true) {
-    gGame.markedCount--
-  }
-    
-    
-    console.log(gGame.markedCount)
-    // console.log(gBoard[i][j])
-    //console.log(cell, i, j);
     checkGameOver()
 }
 
+function isLost() {
 
+    document.querySelectorAll('.cell').forEach(function (cell) {
+        cell.style.color = 'rgb(121, 121, 255)'
+        cell.style.opacity = '100'
+    })
+}
 
 
 
